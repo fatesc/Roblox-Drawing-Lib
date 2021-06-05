270a4ac1c06e4ca24aa779cd348d9d527d9f80d9
fate <66224951+fatesc@users.noreply.github.com>
2021-06-05 01:17:31 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,241 @@
+local Drawing = {}
+newcclosure = newcclosure or function(f)
+    return f
+end
+
+local Workspace = game:GetService("Workspace");
+local Camera = Workspace:FindFirstChild("Camera");
+local CoreGui = game:GetService("CoreGui");
+
+local WorldToScreen = Camera.WorldToScreenPoint
+
+local BaseDrawingProperties = setmetatable({
+    Visible = false,
+    Color = Color3.new(),
+    Transparency = 0,
+    Remove = function()
+    end
+}, {
+    __add = function(tbl1, tbl2)
+        local new = {}
+        for i, v in next, tbl1 do
+            new[i] = v
+        end
+        for i, v in next, tbl2 do
+            new[i] = v
+        end
+        return new
+    end
+})
+
+Drawing.new = function(Type, UI)
+    UI = UI and UI:IsA("ScreenGui") and UI or Instance.new("ScreenGui", CoreGui) or Instance.new("ScreenGui", CoreGui);
+
+    if (Type == "Line") then
+        local LineProperties = ({
+            To = Vector2.new(),
+            From = Vector2.new(),
+            Thickness = 1,
+        } + BaseDrawingProperties)
+
+        local LineFrame = Instance.new("Frame");
+        LineFrame.AnchorPoint = Vector2.new(0.5, 0.5);
+        LineFrame.BorderSizePixel = 0
+
+        LineFrame.BackgroundColor3 = LineProperties.Color
+        LineFrame.Visible = LineProperties.Visible
+        LineFrame.BackgroundTransparency = LineProperties.Transparency
+
+
+        LineFrame.Parent = UI
+
+        return setmetatable({}, {
+            __newindex = newcclosure(function(Instance_, Property, Value)
+                if (Property == "To") then
+                    local To = Value
+                    local Direction = (To - LineProperties.From);
+                    local Center = (To + LineProperties.From) / 2
+                    local Distance = Direction.Magnitude
+                    local Theta = math.atan2(Direction.Y, Direction.X);
+
+                    LineFrame.Position = UDim2.fromOffset(Center.X, Center.Y);
+                    LineFrame.Rotation = math.deg(Theta);
+                    LineFrame.Size = UDim2.fromOffset(Distance, LineProperties.Thickness);
+
+                    LineProperties.To = To
+                end
+                if (Property == "From") then
+                    local From = Value
+                    local Direction = (LineProperties.To - From);
+                    local Center = (LineProperties.To + From) / 2
+                    local Distance = Direction.Magnitude
+                    local Theta = math.atan2(Direction.Y, Direction.X);
+
+                    LineFrame.Position = UDim2.fromOffset(Center.X, Center.Y);
+                    LineFrame.Rotation = math.deg(Theta);
+                    LineFrame.Size = UDim2.fromOffset(Distance, LineProperties.Thickness);
+
+
+                    LineProperties.From = From
+                end
+                if (Property == "Visible") then
+                    LineFrame.Visible = true
+                    LineProperties.Visible = true
+                end
+                if (Property == "Thickness") then
+                    local Direction = (LineProperties.To - LineProperties.From);
+                    local Distance = Direction.Magnitude
+
+                    LineFrame.Size = UDim2.fromOffset(Distance, LineProperties.Thickness);
+
+                    LineProperties.Thickness = Value
+                end
+                if (Property == "Transparency") then
+                    LineFrame.BackgroundTransparency = 1 - Value
+                    LineProperties.Transparency = Value
+                end
+                if (Property == "Color") then
+                    LineFrame.BackgroundColor3 = Value
+                    LineProperties.Color = Value 
+                end
+            end),
+            __index = newcclosure(function(Instance_, Property)
+                if (Property == "Remove") then
+                    return newcclosure(function()
+                        LineFrame:Destroy();
+                    end)
+                end
+            end)
+        })
+    end
+
+    if (Type == "Circle") then
+        local CircleProperties = ({
+            Radius = 150,
+            Filled = false,
+            Position = Vector2.new()
+        } + BaseDrawingProperties)
+
+        local CircleFrame = Instance.new("Frame");
+
+        CircleFrame.AnchorPoint = Vector2.new(0.5, 0.5);
+        CircleFrame.BorderSizePixel = 0
+
+        CircleFrame.BackgroundColor3 = CircleProperties.Color
+        CircleFrame.Visible = CircleProperties.Visible
+        CircleFrame.BackgroundTransparency = CircleProperties.Transparency
+
+        local Corner = Instance.new("UICorner", CircleFrame);
+        Corner.CornerRadius = UDim.new(1, 0);
+        CircleFrame.Size = UDim2.new(0, CircleProperties.Radius, 0, CircleProperties.Radius);
+
+        CircleFrame.Parent = UI
+
+        return setmetatable({}, {
+            __newindex = newcclosure(function(Instance_, Property, Value)
+                if (Property == "Radius") then
+                    CircleFrame.Size = UDim2.new(0, Value, 0, Value);
+                    CircleProperties.Radius = Value
+                end
+                if (Property == "Position") then
+                    CircleFrame.Position = UDim2.new(0, Value.X, 0, Value.Y);
+                    CircleProperties.Position = Value
+                end
+                if (Property == "Filled") then
+                    CircleFrame.BackgroundTransparency = Value == true and 0 or 0.8
+                    CircleProperties.Filled = Value
+                end
+                if (Property == "Color") then
+                    CircleFrame.BackgroundColor3 = Value
+                    CircleProperties.Color = Value
+                end
+                if (Property == "Visible") then
+                    CircleFrame.Visible = Value
+                    CircleProperties.Visible = Value
+                end
+                if (Property == "Transparency") then
+
+                end
+            end),
+            __index = newcclosure(function(Instance_, Property)
+                if (Property == "Remove") then
+                    return newcclosure(function()
+                        CircleFrame:Destroy();
+                    end)
+                end
+                
+                return CircleProperties[Property]
+            end)
+        })
+    end
+
+    if (Type == "Text") then
+        local TextProperties = ({
+            Text = "",
+            Size = 0,
+            Center = false,
+            Outline = false,
+            OutlineColor = Color3.new(),
+            Position = Vector2.new(),
+        } + BaseDrawingProperties)
+
+        local TextLabel = Instance.new("TextLabel");
+
+        TextLabel.AnchorPoint = Vector2.new(0.5, 0.5);
+        TextLabel.BorderSizePixel = 0
+        TextLabel.Size = UDim2.new(0, 200, 0, 50);
+        TextLabel.Font = Enum.Font.SourceSans
+        TextLabel.TextSize = 14
+
+        TextLabel.TextColor3 = TextProperties.Color
+        TextLabel.Visible = TextProperties.Visible
+        TextLabel.BackgroundTransparency = 1
+        TextLabel.TextTransparency = TextProperties.Transparency
+        
+        TextLabel.Parent = UI
+
+        return setmetatable({}, {
+            __newindex = newcclosure(function(Instance_, Property, Value)
+                if (Property == "Text") then
+                    TextLabel.Text = Value
+                    TextProperties.Text = Value
+                end
+                if (Property == "Position") then
+                    TextLabel.Position = UDim2.new(0, Value.X, 0, Value.Y);
+                    TextProperties.Position = Value
+                end
+                if (Property == "Size") then
+                    TextLabel.TextSize = Value
+                    TextProperties.Size = Value
+                end
+                if (Property == "Color") then
+                    TextLabel.TextColor3 = Value
+                    TextProperties.Color = Value
+                end
+                if (Property == "Transparency") then
+                    TextLabel.TextTransparency = Value
+                    TextProperties.Transparency = Value
+                end
+                if (Property == "Visible") then
+                    TextLabel.Visible = Value
+                    TextProperties.Visible = Value
+                end
+                if (Property == "Center") then
+                    TextLabel.Position = Value == true and UDim2.new(0, Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2, 0)
+                    TextProperties.Center = Value
+                end
+            end),
+            __index = newcclosure(function(Instance_, Property)
+                if (Property == "Remove") then
+                    return newcclosure(function()
+                        TextLabel:Destroy();
+                    end)
+                end
+                
+                return TextProperties[Property]
+            end)
+        })
+    end
+end
+
+return Drawing
